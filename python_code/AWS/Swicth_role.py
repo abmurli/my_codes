d4f846a64eb86f8a744d56d43fc45328ef54b59a
AB <murali.ab@sap.com>
2019-02-13 13:36:54 +0530

    organised the files to AWS directory

---
+++ python_code/AWS/Swicth_role.py
@@ -0,0 +1,20 @@
+
+import boto3
+
+sts_client = boto3.client('sts')
+
+assumedRoleObject = sts_client.assume_role(
+    RoleArn="arn:aws:iam::<account_number>:role/<RoleToAssume>",
+    RoleSessionName="AssumeRoleSession1")
+
+credentials = assumedRoleObject['Credentials']
+
+s3_resource = boto3.resource(
+    's3',
+    aws_access_key_id = credentials['AccessKeyId'],
+    aws_secret_access_key = credentials['SecretAccessKey'],
+    aws_session_token = credentials['SessionToken'],
+)
+
+for bucket in s3_resource.buckets.all():
+    print(bucket.name)
