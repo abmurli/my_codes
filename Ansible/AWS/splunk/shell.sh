1cb9aac86713bbda22bcc564630984902fc99654
AB <murali.ab@sap.com>
2019-02-22 19:44:25 +0530

    added splunk installation

--- Ansible/AWS/splunk/shell.sh
+++ Ansible/AWS/splunk/shell.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-bin/splunk start --accept-license --answer-yes --auto-ports --no-prompt -auth admin:changeme
-bin/splunk splunk edit user admin -password W3llmark123### -auth admin:changeme
-bin/splunk set deploy-poll xvlsplunkd2.sharedservices.aws.wellmark.com:8089
+bin/splunk start --accept-license --answer-yes --auto-ports --no-prompt -auth
+bin/splunk splunk edit user admin -password  -auth
+bin/splunk set deploy-poll 
 bin/splunk enable boot-start
 sleep 2
-systemctl restart splunk 
+systemctl restart splunk
