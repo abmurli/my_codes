44f0a81ef4010817e0c31b6b31cac57972f7973c
AB <murali.ab@sap.com>
2019-03-12 19:44:09 +0530

    added splunk role

--- Ansible/AWS/splunk/shell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-bin/splunk start --accept-license --answer-yes --auto-ports --no-prompt -auth
-bin/splunk splunk edit user admin -password  -auth
-bin/splunk set deploy-poll 
-bin/splunk enable boot-start
-sleep 2
-systemctl restart splunk
